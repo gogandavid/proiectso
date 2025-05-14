06a103f6dee7e898edae80751752fbb45a32e4e2
gogandavid <david.gogan@student.upt.ro>
2025-05-14 09:43:09 +0300

    Create calculate_score.c

---
+++ calculate_score.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+typedef struct treasure{
+    int id;
+    char username[32];
+    int value;
+} treasure;
+
+void findtresure(char user[32]){
+
+}
+
+int main(){
+  
+  return 0;
+}
